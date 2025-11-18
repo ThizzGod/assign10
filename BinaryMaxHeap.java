733daa41d5c91f4ac4ee77e17bfb3dd81b64231c
Max Barker <maxbarker26@gmail.com>
2025-11-17 17:29:04 -0900

    Add files and method stubs

---
+++ BinaryMaxHeap.java
@@ -0,0 +1,64 @@
+package assign10;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class BinaryMaxHeap<E> implements PriorityQueue<E> {
+	public BinaryMaxHeap() {
+		
+	}
+	
+	public BinaryMaxHeap(Comparator<? super E> cmp) {
+		
+	}
+	
+	public BinaryMaxHeap(List<? extends E> list) {
+		
+	}
+
+	public BinaryMaxHeap(List<? extends E> list, Comparator<? super E> cmp) {
+		
+	}
+
+	@Override
+	public void add(E item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public E peek() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E extractMax() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}}
---
+++ BinaryMaxHeapTester.java
@@ -0,0 +1,14 @@
+package assign10;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BinaryMaxHeapTester {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
